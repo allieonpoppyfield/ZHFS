7c3e306489de2c356c126c897db66c0b73e1feff
allie <allie@DESKTOP-BPD4NLJ>
2022-07-18 23:57:09 +0300

    КСЕНИЯ!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Database/Sale.cs
+++ Database/Sale.cs
@@ -4,6 +4,6 @@ public class Sale
     public int Id { get; set; }
     public int UserId { get; set; }
     public int ProductId { get; set; } //тут кстати есть вопрос. думаем ли мы о повышении цены в будущем и как это влияет на статистику
-    public User User { get; set; }
-    public Product Product { get; set; }
+    public User? User { get; set; }
+    public Product? Product { get; set; }
 }
